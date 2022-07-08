acc285eb9ab22d7689b78944674cfe0594f436b9
kaksity <daudapona2@gmail.com>
2022-07-08 13:29:10 +0100

    Added a Welcome Controller

--- src/app/http/controllers/V1/welcome.controllers.ts
+++ src/app/http/controllers/V1/welcome.controllers.ts
@@ -1,9 +1,5 @@
 import { Request, Response } from 'express';
-import { inject } from 'inversify';
 import { controller, httpGet, request, response } from 'inversify-express-utils';
-import { TYPES } from '../../../constants';
-import { UtilityService } from '../../../services';
-import { HttpException } from '../../exceptions';
 import { IResponse } from '../../resources';
 @controller('/api/v1')
 export class WelcomeController
